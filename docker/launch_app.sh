8491dfacecc8ea6e93676e06bc45ebd4e0ee7121
Ronan BEBIN <ronan.bebin@insa-rennes.fr>
2020-12-07 22:57:48 +0100

    Ajout dépendances tensorflow depuis npm

--- docker/launch_app.sh
+++
@@ -1,7 +0,0 @@
-# Build backend and launch it on localhost:4444
-
-# Build
-docker build -t ts-app ..
-
-# Launch
-docker run --name ts-app-run --network=host -p 8080:8080 ts-app
--- docker/stop_app.sh
+++
@@ -1,6 +0,0 @@
-# Stop containers
-docker stop ts-app-run
-
-# Delete container and image
-docker rm $(docker ps -a -q) # Remove all stopped containers
-docker rmi ts-app
